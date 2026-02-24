0396e61a3e4c04b2951d15f9aecca88db21b49b3
HoxtonWealth <marketing@hoxtonwealth.com>
2026-02-24 09:40:37 +0400

    Initial commit: consultation booking app

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ConsultationBooking from '../consultation-booking-preview'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConsultationBooking />
+  </React.StrictMode>,
+)
